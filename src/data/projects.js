f2dd3778d05ff67a4664dbb2cba67a6741e9dbfd
arayski <107960750+arayski@users.noreply.github.com>
2023-12-29 14:20:21 -0500

    initial commit

---
+++ src/data/projects.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Class Registration',
+        imgUrl: '/assets/react.svg',
+        stack: ['Vue.js', 'Python', 'AWS'],
+        link: '',
+    },
+    {
+        title: 'Class Registration',
+        imgUrl: '/vite.svg',
+        stack: ['Vue.js', 'Python', 'AWS'],
+        link: '',
+    },
+    {
+        title: 'Class Registration',
+        imgUrl: '',
+        stack: ['Vue.js', 'Python', 'AWS'],
+        link: '',
+    }
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Student',
+        duration: '2 years',
+        details: 'Full time studentfvnm, wfvn er,',
+    },
+
+    {
+        year: 'Today',
+        title: 'Student',
+        duration: '2 years',
+        details: 'Full time studentfvnm, wfvn er,',
+    },
+
+    {
+        year: 'Today',
+        title: 'Student',
+        duration: '2 years',
+        details: 'Full time studentfvnm, wfvn er,',
+    },
+
+    {
+        year: 'Today',
+        title: 'Student',
+        duration: '2 years',
+        details: 'Full time studentfvnm, wfvn er,',
+    }
+]
